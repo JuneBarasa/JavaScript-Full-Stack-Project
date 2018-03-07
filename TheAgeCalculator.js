8b6f9adab8a2fccd4a55f2920a149c5a1f371dc6
June <34090565+JuneBarasa@users.noreply.github.com>
2018-03-07 22:19:43 +0300

    initial commit

---
+++ TheAgeCalculator.js
@@ -0,0 +1,7 @@
+var birthYear = 1996;
+var futureYear = 2065;
+
+var possibleAge1 = futureYear - birthYear;
+var possibleAge2 = (futureYear - birthYear) + 1;
+
+console.log("I will be either " +possibleAge1+ " or " +possibleAge2+ " in YYYY")
