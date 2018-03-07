2b8d897eb1f07f8f504c7540ee88e36c3a0b8134
June <34090565+JuneBarasa@users.noreply.github.com>
2018-03-07 22:30:19 +0300

    initial commit

---
+++ TheLifetimeSupplyCalculator.js
@@ -0,0 +1,7 @@
+var currentAge = 21;
+var maxAge = 70;
+var AmountPerDay = 3;
+
+var total = AmountPerDay * ((maxAge - currentAge) * 365);
+
+console.log("You will need " + total + " to last you until the ripe old age of "+ maxAge)
