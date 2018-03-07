dcf91cea38d159203b85dd3ae835c0398e629c16
June <34090565+JuneBarasa@users.noreply.github.com>
2018-03-07 22:38:19 +0300

    initial commit

---
+++ TheGeometrizer.js
@@ -0,0 +1,6 @@
+var radius = 6;
+var circumference = 2 * Math.PI * radius;
+var area = Math.PI * (radius^2);
+
+console.log("The circumference is " +circumference)
+console.log("The area is " +area)
